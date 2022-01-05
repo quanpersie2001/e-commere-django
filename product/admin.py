ac1bbc43b9a836674d44adc60a6979a647090fc4
quanpersie2001 <quanpersie001@gmail.com>
2022-01-06 00:25:22 +0700

    Fix product image

--- product/admin.py
+++ product/admin.py
@@ -5,8 +5,6 @@ from .models import Image, Product, Category
 # Register your models here.
 class ProductImageAdmin(admin.StackedInline):
     model = Image
-    extra = 0
-    min_num = 3
 
 
 class ProductAdmin(admin.ModelAdmin):
